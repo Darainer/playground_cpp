471664689b041e162d8f1da771fffb57fc6b4545
Darainer <ryanjmatthews@gmail.com>
2021-12-04 18:41:43 +0100

    initial skeleton

---
+++ tracking.cpp
@@ -0,0 +1,20 @@
+#include "object.hpp"
+#include <array>
+
+
+namespace Object_types{
+namespace Object_tracking{
+namespace tracking_application{
+
+
+
+} // namespace tracking_application
+} // namespace Object_tracking
+} // namespace Object_types
+
+int init(){
+//std::array<Object,64> ObjectList {};  // how to allocate? 
+    return 0; 
+}
+
+int main(){return 0;};
